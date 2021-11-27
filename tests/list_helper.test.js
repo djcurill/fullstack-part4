1b2be7a4994e544ea3e44737bc9e14ac0e6ee746
Dan Curilla <curilladaniel@gmail.com>
2021-11-26 18:46:04 -0700

    Excercise 4.4 - 4.7 complete

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,5 +1,29 @@
 const listHelper = require('../utils/list_helper');
 
+const noBlogs = [];
+const oneBlog = [
+  {
+    title: 'blog #1',
+    author: 'louis the first',
+    url: 'blog.uno',
+    likes: 42,
+  },
+];
+const twoBlogs = [
+  {
+    title: 'blog #1',
+    author: 'louis the first',
+    url: 'blog.uno',
+    likes: 1,
+  },
+  {
+    title: 'blog #2',
+    author: 'louis the second',
+    url: 'blog.dos',
+    likes: 2,
+  },
+];
+
 describe('dummy', () => {
   test('dummy returns one', () => {
     const blogs = [];
@@ -8,34 +32,8 @@ describe('dummy', () => {
 });
 
 describe('totalLikes', () => {
-  const noBlogs = [];
-  const oneBlog = [
-    {
-      title: 'blog #1',
-      author: 'louis the first',
-      url: 'blog.uno',
-      likes: 42,
-    },
-  ];
-  const multipleBlogs = [
-    {
-      title: 'blog #1',
-      author: 'louis the first',
-      url: 'blog.uno',
-      likes: 1,
-    },
-    {
-      title: 'blog #2',
-      author: 'louis the second',
-      url: 'blog.dos',
-      likes: 2,
-    },
-  ];
-
   test('totalLikes returns 0 for no blogs', () => {
-    const blogs = [];
-
-    expect(listHelper.totalLikes(blogs)).toBe(0);
+    expect(listHelper.totalLikes(noBlogs)).toBe(0);
   });
 
   test('when list has one blog, totalLikes equals likes of that blog', () => {
@@ -43,6 +41,121 @@ describe('totalLikes', () => {
   });
 
   test('when list has multiple blogs, totalLikes equals sum of all likes', () => {
-    expect(listHelper.totalLikes(multipleBlogs)).toBe(3);
+    expect(listHelper.totalLikes(twoBlogs)).toBe(3);
+  });
+});
+
+describe('argMax', () => {
+  test('argMax is 0 if [3,2,1]', () => {
+    expect(listHelper.argMax([3, 2, 1])).toBe(0);
+  });
+
+  test('argMax is 0 if array is empty', () => {
+    expect(listHelper.argMax([])).toBe(0);
+  });
+
+  test('argMax is 0 if array has one value', () => {
+    expect(listHelper.argMax([1])).toBe(0);
+  });
+
+  test('argMax any index given multiple maximums', () => {
+    const result = listHelper.argMax([1, 2, 3, 3, 2, 1]);
+    const answer = result === 2 || result === 3;
+    expect(answer).toBe(true);
+  });
+});
+
+describe('favoriteBlog', () => {
+  test('returns null for empty array', () => {
+    expect(listHelper.favoriteBlog(noBlogs)).toBe(null);
+  });
+
+  test('returns first value of array with one blog', () => {
+    expect(listHelper.favoriteBlog(oneBlog)).toEqual(oneBlog[0]);
+  });
+
+  test('returns blog with most likes given array with many blogs', () => {
+    expect(listHelper.favoriteBlog(twoBlogs)).toEqual(twoBlogs[1]);
+  });
+
+  describe('mostBlogs', () => {
+    const manyBlogs = [
+      {
+        title: 'blog #1',
+        author: 'bob',
+        url: 'blog.uno',
+        likes: 1,
+      },
+      {
+        title: 'blog #2',
+        author: 'bob',
+        url: 'blog.dos',
+        likes: 2,
+      },
+      {
+        title: 'blog #3',
+        author: 'mary',
+        url: 'blog.tres',
+        likes: 1,
+      },
+      {
+        title: 'blog #4',
+        author: 'bob',
+        url: 'blog.quatro',
+        likes: 2,
+      },
+    ];
+    test('returns null for empty array', () => {
+      expect(listHelper.mostBlogs(noBlogs)).toBe(null);
+    });
+
+    test('returns only author in array of one blog', () => {
+      expect(listHelper.mostBlogs(oneBlog)).toBe('louis the first');
+    });
+
+    test('returns author with most blogs given many blogs', () => {
+      expect(listHelper.mostBlogs(manyBlogs)).toBe('bob');
+    });
+  });
+});
+
+describe('mostLikes', () => {
+  const manyBlogs = [
+    {
+      title: 'blog #1',
+      author: 'bob',
+      url: 'blog.uno',
+      likes: 1,
+    },
+    {
+      title: 'blog #2',
+      author: 'bob',
+      url: 'blog.dos',
+      likes: 2,
+    },
+    {
+      title: 'blog #3',
+      author: 'mary',
+      url: 'blog.tres',
+      likes: 50,
+    },
+    {
+      title: 'blog #4',
+      author: 'bob',
+      url: 'blog.quatro',
+      likes: 2,
+    },
+  ];
+
+  test('given empty array, returns null', () => {
+    expect(listHelper.mostLikes(noBlogs)).toBe(null);
+  });
+
+  test('given array of one blog, returns only author', () => {
+    expect(listHelper.mostLikes(oneBlog)).toBe('louis the first');
+  });
+
+  test('given array of many blogs, author with most lieks is returned', () => {
+    expect(listHelper.mostLikes(manyBlogs)).toBe('mary');
   });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,78 @@
+const _ = require('lodash');
+
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) =>
   // eslint-disable-next-line no-param-reassign
   blogs.reduce((accu, blog) => (accu += blog.likes), 0);
 
-module.exports = { dummy, totalLikes };
+const argMax = (arr) => {
+  if (arr.length <= 1) return 0;
+  return arr
+    .map((val, idx) => [val, idx])
+    .reduce((accu, next) => (next[0] > accu[0] ? next : accu))[1];
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+  if (blogs.length === 1) return blogs[0];
+  const idx = argMax(blogs.map((b) => b.likes));
+  return blogs[idx];
+};
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null;
+  if (blogs.length === 1) return blogs[0].author;
+  return _(blogs).countBy('author').entries().maxBy(_.last)[0];
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return null;
+  if (blogs.length === 1) return blogs[0].author;
+  return _(blogs)
+    .groupBy('author')
+    .map((summary, name) => ({
+      author: name,
+      totalLikes: _.sumBy(summary, 'likes'),
+    }))
+    .maxBy('totalLikes').author;
+};
+
+const manyBlogs = [
+  {
+    title: 'blog #1',
+    author: 'bob',
+    url: 'blog.uno',
+    likes: 1,
+  },
+  {
+    title: 'blog #2',
+    author: 'bob',
+    url: 'blog.dos',
+    likes: 2,
+  },
+  {
+    title: 'blog #3',
+    author: 'mary',
+    url: 'blog.tres',
+    likes: 50,
+  },
+  {
+    title: 'blog #4',
+    author: 'bob',
+    url: 'blog.quatro',
+    likes: 2,
+  },
+];
+
+console.log(mostLikes(manyBlogs));
+
+module.exports = {
+  dummy,
+  totalLikes,
+  argMax,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+};
